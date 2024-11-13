3c798b5b70fac56183aec9a3f69f49e4441785d7
tunguyen1993 <s2.shining93@gmail.com>
2024-11-13 09:50:43 +0700

    Update pdf.controller.ts

--- src/modules/api/pdf/pdf.controller.ts
+++ src/modules/api/pdf/pdf.controller.ts
@@ -24,52 +24,56 @@ class PdfController extends BaseController {
     }
 
     @Post('/upload', {response: {}})
-    async show({body}: { body: uploadFilePdf }) {
-
-        const baseDir = "public/storage/";
-        const folderName = crypto.randomUUID();
-        const outputDir = path.join(process.cwd(), `public/storage/${folderName}`);
-        const pdfPath = `${baseDir}${crypto.randomUUID()}.png`;
-        await Bun.write(pdfPath, body.file);
-
-        if (!fs.existsSync(outputFolder)) {
-            fs.mkdirSync(outputFolder, { recursive: true });
-        }
-
-
-        const options = {
-            density: 500, // Độ phân giải ảnh
-            savePath: outputFolder, // Thư mục lưu ảnh
-            format: "png", // Định dạng ảnh (png, jpeg)
-            width: 600, // Chiều rộng ảnh
-            height: 800 // Chiều cao ảnh
-        };
-        const pdfToPic = pdf2pic.fromPath(pdfPath, options);
-
-        // Lấy số trang từ file PDF
-        const pdfBytes = await fs.promises.readFile(pdfPath);
-        const pdfDoc = await PDFDocument.load(pdfBytes);
-        const totalPages = pdfDoc.getPageCount();
-
-        // Chuyển đổi từng trang PDF thành ảnh
-        const promises = [];
-        for (let page = 1; page <= totalPages; page++) {
-            promises.push(
-                pdfToPic(page).then((result) => {
-                    console.log(`Trang ${page} đã được chuyển đổi: ${result}`);
-                }).catch((error) => {
-                    console.error(`Lỗi khi chuyển đổi trang ${page}:`, error);
+     async show({body}: { body: uploadFilePdf }) {
+            try{
+                const baseDir = "public/storage/";
+                const folderName = crypto.randomUUID();
+                const outputDir = path.join(process.cwd(), `public/storage/${folderName}`);
+                const pdfPath = `${baseDir}${crypto.randomUUID()}.png`;
+                await Bun.write(pdfPath, body.file);
+    
+                if (!fs.existsSync(outputDir)) {
+                    fs.mkdirSync(outputDir, { recursive: true });
+                }
+    
+    
+                const options = {
+                    density: 500, // Độ phân giải ảnh
+                    savePath: outputDir, // Thư mục lưu ảnh
+                    format: "png", // Định dạng ảnh (png, jpeg)
+                    width: 600, // Chiều rộng ảnh
+                    height: 800 // Chiều cao ảnh
+                };
+                const pdfToPic = pdf2pic.fromPath(pdfPath, options);
+    
+                // Lấy số trang từ file PDF
+                const pdfBytes = await fs.promises.readFile(pdfPath);
+                const pdfDoc = await PDFDocument.load(pdfBytes);
+                const totalPages = pdfDoc.getPageCount();
+    
+                // Chuyển đổi từng trang PDF thành ảnh
+                const promises = [];
+                for (let page = 1; page <= totalPages; page++) {
+                    promises.push(
+                        pdfToPic(page).then((result) => {
+                            console.log(`Trang ${page} đã được chuyển đổi: ${result}`);
+                        }).catch((error) => {
+                            console.error(`Lỗi khi chuyển đổi trang ${page}:`, error);
+                        })
+                    );
+                }
+    
+                const results: images = await Promise.all(promises);
+                results.map((image: image) => {
+                    image.path = `${outputDir}/${image.name}`;
+                    return image;
                 })
-            );
-        }
-
-        const results: images = await Promise.all(promises);
-        results.map((image: image) => {
-            image.path = `/public/storage/${outputFolder}/${image.name}`;
-            return image;
-        })
-        return { message: results };
-    }
+                return { message: results };
+            }catch (e){
+                console.log(e)
+                return {};
+            }
+     }
 
     @Get('/view',  {response: {}})
     async view(ctx) {
